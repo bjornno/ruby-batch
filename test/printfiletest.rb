b904976ea808f145e6e8b2b092749687ef4f8388
bjn <bjornno@gmail.com>
2008-11-23 17:13:38 +0100

    added test

---
+++ test/printfiletest.rb
@@ -0,0 +1,17 @@
+require '../rubybatch'
+
+filescan 'data/from_host/bjorn', 'bjorn*' do
+  |file| 
+  if file != nil
+    puts '---------------'
+    puts 'reading file'
+    while line = file.gets   
+      if (line != nil)
+        puts line   
+      end
+    end  
+  end
+  puts 'finished reading file'
+  puts '---------------'
+  
+end
